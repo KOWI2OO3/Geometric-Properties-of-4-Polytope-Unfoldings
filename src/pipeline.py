42d591201eaf01138f2d907027cb2eda5df2fe56
Koen Winkel <2003KOWI@gmail.com>
2026-06-05 12:51:25 +0200

    Simplified experiment selection

--- src/pipeline.py
+++ src/pipeline.py
@@ -21,8 +21,8 @@ from net import Net, constructAdjacencyGraph
 # m_instances = 10
 
 # Experiment 3 V2
-n_samples = 10_000
-m_instances = 10
+# n_samples = 10_000
+# m_instances = 10
 
 # Debug
 # n_samples = 1
@@ -38,7 +38,7 @@ def log(logger, context):
     if log_to_console:
         print(line)
 
-def run_pipeline(min, max, step: float, func):
+def run_pipeline(min, max, step: float, func, m_instances: int, n_samples: int):
     startTime = datetime.now()
     with open("output/log.txt", "w") as logger:
         with open("output/results.txt", "w") as results:
@@ -54,7 +54,7 @@ def run_pipeline(min, max, step: float, func):
                 for m in range(m_instances):
                     log(logger, "Computating polytopes with " + str(i) + " #" + str(m + 1))
                     hull = func(i)
-                    success_rate = pipeline_sample(hull, logger)
+                    success_rate = pipeline_sample(hull, logger, n_samples)
                     results.write(str(i) + ", " + str(success_rate))
                     results.write(",\n")
                 log(logger, "Finished polytopes with " + str(i))
@@ -64,7 +64,7 @@ def run_pipeline(min, max, step: float, func):
             
         log(logger, "Computation finished! took " + str(datetime.now() - startTime))
 
-def pipeline_sample(hull, logger):
+def pipeline_sample(hull, logger, n_samples: int):
     graph, valid_indices = constructAdjacencyGraph(hull)
     
     n_success: int = 0
@@ -82,10 +82,10 @@ def pipeline_sample(hull, logger):
     return float(n_success) / float(n_samples)
 
 # Experiment 1
-# run_pipeline(5, 30, 1, lambda x: gen.generate_clustering_unit_convex(x, 0))
+# run_pipeline(5, 30, 1, lambda x: gen.generate_clustering_unit_convex(x, 0), 5, 1000)
 
 # Experiment 2 ~ 16 hours
-# run_pipeline(0, 1, 0.05, lambda x: gen.generate_clustering_unit_convex(12, x))
+# run_pipeline(0, 1, 0.05, lambda x: gen.generate_clustering_unit_convex(12, x), 10, 10_000)
 
 # Experiment 3 ~ 16 hours
-run_pipeline(1, 20, 1, lambda x: gen.generate_convex_with_deformation(10, np.array([x, 1, 1, 1])))
+# run_pipeline(1, 20, 1, lambda x: gen.generate_convex_with_deformation(10, np.array([x, 1, 1, 1])), 10, 10_000)
